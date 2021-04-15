b65214ebaf57ab8f8ffe0f16bfccbd398b5c5e9c
Excel Chukwu <57713506+MimicTester1307@users.noreply.github.com>
2021-04-15 21:53:54 +0000

    Update TEAM30.sql

--- TEAM30.sql
+++ TEAM30.sql
@@ -232,27 +232,17 @@ CREATE table outpatient(
     foreign key (patient_id) references patient(patient_id),
     foreign key (doctor_id) references doctor(doctor_id)
 );
-insert into outpatient (patient_id, date_of_discharge, doctor_id)values(05001
+insert into outpatient (patient_id, date_of_discharge, doctor_id)values(05001,2011-10-3,01011);
+insert into outpatient (patient_id, date_of_discharge, doctor_id)values(05002,2023-12-2,01022);
+insert into outpatient (patient_id, date_of_discharge, doctor_id)values(05003,2012-01-4,01031);
+insert into outpatient (patient_id, date_of_discharge, doctor_id)values(05004,2011-11-2,01042);
+insert into outpatient (patient_id, date_of_discharge, doctor_id)values(05005,2000-04-5,01045);
+insert into outpatient (patient_id, date_of_discharge, doctor_id)values(05006,2019-05-8,01012);
+insert into outpatient (patient_id, date_of_discharge, doctor_id)values(05007,2011-12-11,01023);
+insert into outpatient (patient_id, date_of_discharge, doctor_id)values(05008,2002-04-4,01034);
+insert into outpatient (patient_id, date_of_discharge, doctor_id)values(05009,2002-2-5,01065);
+insert into outpatient (patient_id, date_of_discharge, doctor_id)values(05010,2021-9-9,01075);
 
-,2011-10-3,01011);
-insert into outpatient (patient_id, date_of_discharge, doctor_id)values(05002
-,2023-12-2,01022);
-insert into outpatient (patient_id, date_of_discharge, doctor_id)values(05003
-,2012-01-4,01031);
-insert into outpatient (patient_id, date_of_discharge, doctor_id)values(05004
-,2011-11-2,01042);
-insert into outpatient (patient_id, date_of_discharge, doctor_id)values(05005
-,2000-04-5,01045);
-insert into outpatient (patient_id, date_of_discharge, doctor_id)values(05006
-,2019-05-8,01012);
-insert into outpatient (patient_id, date_of_discharge, doctor_id)values(05007
-,2011-12-11,01023);
-insert into outpatient (patient_id, date_of_discharge, doctor_id)values(05008
-,2002-04-4,01034);
-insert into outpatient (patient_id, date_of_discharge, doctor_id)values(05009
-,2002-2-5,01065);
-insert into outpatient (patient_id, date_of_discharge, doctor_id)values(05010
-,2021-9-9,01075);
 -- Blood Bank table
 CREATE TABLE bloodbank(
 	donor_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
